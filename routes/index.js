98f20ba92011d3d9abc7533eb4da7cbac10bbdbc
Michele Schio <schio.michele@gmail.com>
2016-09-04 23:18:54 +0200

    navbar icone centrate, aggiunta user page

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,18 @@ var router= express.Router();
 router.use(bodyParser.json());
 router.use('/public', assets);
 
+
 router.route('/').get(function(req, res){
   res.render('./app');
 });
 router.route('/map').get(function(req, res){
-  res.render('./bike');
+  res.render('./map');
 });
 router.route('/about').get(function(req, res){
   res.render('./about');
+});
+router.route('/user').get(function(req, res){
+  res.render('./user');
 });
 router.route('/contact').get(function(req, res){
   res.render('./contact');
