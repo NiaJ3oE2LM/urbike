0cf3d02cce9bd62e42cd24a68d99f56fd11bdb79
Michele Schio <schio.michele@gmail.com>
2016-09-25 21:35:35 +0200

    manca form login da fare

--- public/userCtrl.js
+++ public/userCtrl.js
@@ -2,4 +2,27 @@
 angular.module('starter')
 .controller('userCtrl',['$scope', '$http', function($scope, $http){
 
+  $scope.pin;
+  $scope.bikeId=1;
+  $scope.userId=2;
+
+  function chiamata() {
+    console.log($scope.pin);
+    $http({
+    method: 'POST',
+    data:{"userId":$scope.userId, "bikeId":$scope.bikeId, "pin":$scope.pin},
+    url: '/app/map',
+  }).then(function successCallback(response) {
+      console.log(response);
+    }, function errorCallback(response) {
+      console.log(response);
+    });
+  }
+
+  $('#test').on('click', function () {
+   var $btn = $(this);
+   chiamata();
+   $btn.button('reset');
+ })
+
 }])
